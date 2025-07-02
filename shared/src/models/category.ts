ebb4deae9cdaeb5c752f683374b1979f6993af5b
Grant Colley <grantcolley@yahoo.com>
2025-07-02 22:04:02 +0100

    Update category.ts

--- shared/src/models/category.ts
+++ shared/src/models/category.ts
@@ -1,14 +1,15 @@
 import { Editability } from "../interfaces/editability";
-import { Visibility } from "../interfaces/visibility";
+import { Permissionable } from "../interfaces/permissionable";
 import { Page } from "./page";
 
-export class Category implements Editability, Visibility {
+export class Category implements Editability, Permissionable {
   categoryId: number;
   moduleId: number;
   name: string;
   icon: string;
   isReadonlOnly: boolean;
   isVisible: boolean;
+  permission: string;
   pages: Page[];
 
   constructor(
@@ -18,6 +19,7 @@ export class Category implements Editability, Visibility {
     icon: string,
     isReadonlOnly: boolean,
     isVisible: boolean,
+    permission: string,
     pages: Page[] = []
   ) {
     this.categoryId = categoryId;
@@ -26,6 +28,7 @@ export class Category implements Editability, Visibility {
     this.icon = icon;
     this.isReadonlOnly = isReadonlOnly;
     this.isVisible = isVisible;
+    this.permission = permission;
     this.pages = pages;
   }
 
