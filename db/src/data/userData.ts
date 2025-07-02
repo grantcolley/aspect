53324c52b3b2a5c6c64719bdffe07c094f455570
Grant Colley <grantcolley@yahoo.com>
2025-07-02 23:26:05 +0100

    Update userData.ts

--- db/src/data/userData.ts
+++ db/src/data/userData.ts
@@ -2,7 +2,7 @@ import { User } from "shared/src/models/user";
 
 export function getUsers() {
   return [
-    new User(1, "Alice", "alice@email.com", false),
-    new User(2, "Grant", "grant@email.com", false),
+    new User(1, "Alice", "alice@email.com", "auth_rw"),
+    new User(2, "Bob", "bob@email.com", "auth_ro"),
   ];
 }
