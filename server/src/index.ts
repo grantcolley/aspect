26e68c3acc3d2be64c829ea256db8777f13164e5
Grant Colley <grantcolley@yahoo.com>
2025-07-05 22:53:11 +0100

    Enable CORS #11

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,16 @@
 import express from "express";
+import cors from "cors";
 import { User } from "shared";
 
 const app = express();
 const port = 3000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // or use '*' for all origins (not recommended for production)
+    credentials: true, // if you're using cookies or HTTP auth
+  })
+);
 
 app.get("/api/user", (req, res) => {
   const user: User = { id: 1, name: "Alice" };
