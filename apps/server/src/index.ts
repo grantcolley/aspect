68779110a9f27c7938d5be9999a34fd405411d5c
Grant Colley <grantcolley@yahoo.com>
2025-07-15 21:25:11 +0100

    Update the index with new routes #18 #17

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -9,6 +9,8 @@ import permissionsRouter from "./routes/permissions";
 import rolesRouter from "./routes/roles";
 import usersRouter from "./routes/users";
 import pagesRouter from "./routes/pages";
+import categoriesRouter from "./routes/categories";
+import modulesRouter from "./routes/modules";
 
 const env = process.env.NODE_ENV || "development";
 
@@ -56,6 +58,14 @@ if (!process.env.ENDPOINT_PAGES) {
   throw new Error("ENDPOINT_PAGES environment variable is not set");
 }
 
+if (!process.env.ENDPOINT_CATEGORIES) {
+  throw new Error("ENDPOINT_CATEGORIES environment variable is not set");
+}
+
+if (!process.env.ENDPOINT_MODULES) {
+  throw new Error("ENDPOINT_MODULES environment variable is not set");
+}
+
 const authAudience = process.env.AUTH_AUDIENCE;
 const authIssuerBaseURL = process.env.AUTH_ISSUER_BASE_URL;
 const authTokenSigningAlg = process.env.AUTH_TOKEN_SIGNING_ALGORITHM;
@@ -64,6 +74,8 @@ const permissionsEndpoint = process.env.ENDPOINT_PERMISSIONS;
 const rolesEndpoint = process.env.ENDPOINT_ROLES;
 const usersEndpoint = process.env.ENDPOINT_USERS;
 const pagesEndpoint = process.env.ENDPOINT_PAGES;
+const categoriesEndpoint = process.env.ENDPOINT_CATEGORIES;
+const modulesEndpoint = process.env.ENDPOINT_MODULES;
 
 const app = express();
 app.use(express.json());
@@ -92,6 +104,8 @@ const start = async () => {
   app.use(rolesEndpoint, rolesRouter);
   app.use(usersEndpoint, usersRouter);
   app.use(pagesEndpoint, pagesRouter);
+  app.use(categoriesEndpoint, categoriesRouter);
+  app.use(modulesEndpoint, modulesRouter);
 
   // handle all exceptions
   app.use(errorHandler);
