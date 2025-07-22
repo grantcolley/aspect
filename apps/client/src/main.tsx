87c5691593162cdb800162000fea520add72af7b
Grant Colley <grantcolley@yahoo.com>
2025-07-22 19:03:10 +0100

    Update main.tsx

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "@/components/layout/theme-provider";
-import Auth0ProviderWithNavigate from "@/components/layout/auth0-provider-with-navigate.tsx";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Auth0ProviderWithNavigate>
-        <ThemeProvider defaultTheme="system" storageKey="aspect-ui-theme">
-          <App />
-        </ThemeProvider>
-      </Auth0ProviderWithNavigate>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
