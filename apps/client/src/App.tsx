646cb20e1baa47185daa4130d96bec983101b585
Grant Colley <grantcolley@yahoo.com>
2025-07-22 08:40:05 +0100

    Update App.tsx

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,29 +1,26 @@
 import { AppSidebar } from "@/components/layout/app-sidebar";
 import { AppSidebarHeader } from "@/components/layout/app-sidebar-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { ThemeProvider } from "@/components/layout/theme-provider";
 import "./App.css";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="system" storageKey="aspect-ui-theme">
-      <SidebarProvider
-        style={
-          {
-            "--sidebar-width": "calc(var(--spacing) * 72)",
-            "--header-height": "calc(var(--spacing) * 12)",
-          } as React.CSSProperties
-        }
-      >
-        <AppSidebar variant="inset" />
-        <SidebarInset>
-          <AppSidebarHeader />
-          <div className="flex flex-1 flex-col">
-            <div className="@container/main flex flex-1 flex-col gap-2"></div>
-          </div>
-        </SidebarInset>
-      </SidebarProvider>
-    </ThemeProvider>
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <AppSidebarHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2"></div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
 
