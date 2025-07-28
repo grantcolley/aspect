f8241693bbcdf931924ec279e14a4418506ab3bc
Grant Colley <grantcolley@yahoo.com>
2025-07-28 08:06:39 +0100

    Create not-found.tsx

---
+++ apps/client/src/pages/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => (
+  <>
+    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight mt-20">
+      Oops!
+    </h4>
+    <p className="text-muted-foreground text-xl">Nothing to see here...</p>
+    <p className="text-muted-foreground text-sm">404 - Not Found</p>
+  </>
+);
+export default NotFound;
