9b9ba5087bb0292accbe276f4964b083d88c991f
Grant Colley <grantcolley@yahoo.com>
2025-07-28 08:06:45 +0100

    Create fetch-modules.ts

---
+++ apps/client/src/requests/fetch-modules.ts
@@ -0,0 +1,20 @@
+import { config } from "@/config/config";
+import { Module } from "shared/src/models/module";
+
+export const fetchModules = async (token: string) => {
+  const navigationUrl = `${config.API_URL}/${config.API_NAVIGATION_URL}`;
+
+  const response = await fetch(navigationUrl, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data: Module[] = await response.json();
+
+  return data;
+};
