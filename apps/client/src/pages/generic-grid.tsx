3efc69b3fb77a690ed50dc6a4ce0aa9f3dc23777
Grant Colley <grantcolley@yahoo.com>
2025-08-10 20:03:46 +0100

    Delete generic-grid.tsx

--- apps/client/src/pages/generic-grid.tsx
+++
@@ -1,8 +0,0 @@
-import { useLocation, type Location } from "react-router-dom";
-
-export default function GenericGrid() {
-  const location: Location = useLocation();
-  return (
-    <div className="text-red-500">GenericGrid for {location.pathname}</div>
-  );
-}
