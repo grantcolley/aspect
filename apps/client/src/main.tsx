3fe25a78f04f719eddfe0f1ed7b16a9bc0eac3ec
Grant Colley <grantcolley@yahoo.com>
2025-08-14 09:10:59 +0100

    Update main.tsx

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@/components/layout/theme-provider";
-import Auth0ProviderApp from "@/auth/auth0-provider-app.tsx";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="aspect-ui-theme">
-      <Auth0ProviderApp>
-        <App />
-      </Auth0ProviderApp>
+      <App />
     </ThemeProvider>
   </StrictMode>
 );
