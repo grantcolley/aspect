498a29338d77aac8fd846c871ddf6b78f1ceee56
Grant Colley <grantcolley@yahoo.com>
2025-08-20 13:06:14 +0100

    Update main.tsx

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,13 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { config } from "@/config/config";
 import { ThemeProvider } from "@/components/layout/theme-provider";
+import { router } from "./router";
 import "./index.css";
-import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="aspect-ui-theme">
-      <App />
+      <Auth0Provider
+        domain={config.AUTH0_DOMAIN}
+        clientId={config.AUTH0_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+          audience: config.AUTH0_AUDIENCE || undefined,
+        }}
+        onRedirectCallback={(appState) => {
+          router.navigate(appState?.returnTo || window.location.pathname);
+        }}
+      >
+        <RouterProvider router={router} />
+      </Auth0Provider>
     </ThemeProvider>
   </StrictMode>
 );
