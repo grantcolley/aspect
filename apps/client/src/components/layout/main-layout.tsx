3c21a6c64c70f5a330095034102d3efe29ebf71a
Grant Colley <grantcolley@yahoo.com>
2025-08-20 12:34:42 +0100

    Update main-layout.tsx

--- apps/client/src/components/layout/main-layout.tsx
+++ apps/client/src/components/layout/main-layout.tsx
@@ -3,7 +3,6 @@ import { AppSidebar } from "@/components/layout/app-sidebar";
 import { AppSidebarHeader } from "@/components/layout/app-sidebar-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { Module } from "shared/src/models/module";
-import Auth0ProviderWithNavigate from "@/auth/auth0-provider-with-navigate";
 
 type Props = {
   modules: Module[];
@@ -11,25 +10,23 @@ type Props = {
 
 export const MainLayout = ({ modules }: Props) => {
   return (
-    <Auth0ProviderWithNavigate>
-      <SidebarProvider
-        style={
-          {
-            "--sidebar-width": "calc(var(--spacing) * 72)",
-            "--header-height": "calc(var(--spacing) * 12)",
-          } as React.CSSProperties
-        }
-      >
-        <AppSidebar modules={modules} variant="inset" />
-        <SidebarInset>
-          <AppSidebarHeader />
-          <div className="flex flex-1 flex-col">
-            <div className="@container/main flex flex-1 flex-col gap-2">
-              <Outlet />
-            </div>
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar modules={modules} variant="inset" />
+      <SidebarInset>
+        <AppSidebarHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <Outlet />
           </div>
-        </SidebarInset>
-      </SidebarProvider>
-    </Auth0ProviderWithNavigate>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 };
