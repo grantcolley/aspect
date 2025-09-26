79cb75f410796700bba8fafe64b5a631808bb004
Grant Colley <grantcolley@yahoo.com>
2025-09-26 08:03:38 +0100

    Create constants.ts

---
+++ apps/shared/src/constants/constants.ts
@@ -0,0 +1,23 @@
+export const MODELS = {
+  MODULE: "Module",
+  CATEGORY: "Category",
+  PAGE: "Page",
+  USER: "User",
+  ROLE: "Role",
+  PERMISSION: "Permission",
+};
+
+export const PERMISSIONS = {
+  ADMIN_RO: "admin_ro",
+  ADMIN_RW: "admin_rw",
+};
+
+export const ROLES = {
+  ADMIN: "admin",
+  AUTH: "auth",
+};
+
+export const COMPONENTS = {
+  GENERIC_DATA_TABLE: "GenericDataTable",
+  GENERIC_MODEL_FORM: "GenericModelForm",
+};
