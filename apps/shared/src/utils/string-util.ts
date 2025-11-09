4f5e121d378a1599184ef46d7a2b310231193513
Grant Colley <grantcolley@yahoo.com>
2025-11-09 09:26:02 +0000

    Update string-util.ts

--- apps/shared/src/utils/string-util.ts
+++ apps/shared/src/utils/string-util.ts
@@ -17,10 +17,18 @@ export function ParseKeyValueString(
     }, {});
 }
 
-export function ParseValueString(value: string): string[] {
+export function ParseStringByComma(value: string): string[] {
   if (!value) return [];
   return value
     .split(",")
+    .map((v) => v.trim())
+    .filter((v) => v.length > 0);
+}
+
+export function ParseStringByPipe(value: string): string[] {
+  if (!value) return [];
+  return value
+    .split("|")
     .map((v) => v.trim())
     .filter((v) => v.length > 0);
 }
