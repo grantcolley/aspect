b3b776197b1c5341c831aae3aab88c432321da09
Grant Colley <grantcolley@yahoo.com>
2025-11-19 07:46:35 +0000

    Update userData.ts
    #29

--- db/src/data/userData.ts
+++ db/src/data/userData.ts
@@ -7,28 +7,21 @@ export function getUsers(roles: Role[]) {
   alice.userId = 1;
   alice.name = "Alice";
   alice.email = "alice@email.com";
-  alice.permission =
-    PERMISSIONS.ACCOUNTS_READ + "|" + PERMISSIONS.ACCOUNTS_WRITE;
 
   let bob = new User();
   bob.userId = 2;
   bob.name = "Bob";
   bob.email = "bob@email.com";
-  bob.permission = PERMISSIONS.ACCOUNTS_READ + "|" + PERMISSIONS.ACCOUNTS_WRITE;
 
   let jane = new User();
   jane.userId = 3;
   jane.name = "Jane";
   jane.email = "jane@email.com";
-  jane.permission =
-    PERMISSIONS.ACCOUNTS_READ + "|" + PERMISSIONS.ACCOUNTS_WRITE;
 
   let will = new User();
   will.userId = 4;
   will.name = "Will";
   will.email = "will@email.com";
-  will.permission =
-    PERMISSIONS.ACCOUNTS_READ + "|" + PERMISSIONS.ACCOUNTS_WRITE;
 
   alice.roles.push(roles[1]); // Assigning 'admin writer' role to Alice
   bob.roles.push(roles[3]); // Assigning 'accounts writer' role to Bob
