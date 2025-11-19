bc9da337107f19855847d043df4c3e885adaca53
Grant Colley <grantcolley@yahoo.com>
2025-11-19 07:44:48 +0000

    Update roleData.ts
    #29

--- db/src/data/roleData.ts
+++ db/src/data/roleData.ts
@@ -9,73 +9,60 @@ export function getRoles() {
     {
       roleId: 1,
       name: ROLES.ADMIN_READER,
-      permission: PERMISSIONS.ADMIN_READ + "|" + PERMISSIONS.ADMIN_WRITE,
       permissions: [
         {
           permissionId: 1,
           name: PERMISSIONS.ADMIN_READ,
-          permission: PERMISSIONS.ADMIN_READ + "|" + PERMISSIONS.ADMIN_WRITE,
         },
         {
           permissionId: 3,
           name: PERMISSIONS.ACCOUNTS_READ,
-          permission: PERMISSIONS.ADMIN_READ + "|" + PERMISSIONS.ADMIN_WRITE,
         },
       ],
     },
     {
       roleId: 2,
       name: ROLES.ADMIN_WRITER,
-      permission: PERMISSIONS.ADMIN_READ + "|" + PERMISSIONS.ADMIN_WRITE,
       permissions: [
         {
           permissionId: 1,
           name: PERMISSIONS.ADMIN_READ,
-          permission: PERMISSIONS.ADMIN_READ + "|" + PERMISSIONS.ADMIN_WRITE,
         },
         {
           permissionId: 2,
           name: PERMISSIONS.ADMIN_WRITE,
-          permission: PERMISSIONS.ADMIN_READ + "|" + PERMISSIONS.ADMIN_WRITE,
         },
         {
           permissionId: 3,
           name: PERMISSIONS.ACCOUNTS_READ,
-          permission: PERMISSIONS.ADMIN_READ + "|" + PERMISSIONS.ADMIN_WRITE,
         },
         {
           permissionId: 4,
           name: PERMISSIONS.ACCOUNTS_WRITE,
-          permission: PERMISSIONS.ADMIN_READ + "|" + PERMISSIONS.ADMIN_WRITE,
         },
       ],
     },
     {
       roleId: 3,
       name: ROLES.ACCOUNTS_READER,
-      permission: PERMISSIONS.ADMIN_READ + "|" + PERMISSIONS.ADMIN_WRITE,
       permissions: [
         {
           permissionId: 3,
           name: PERMISSIONS.ACCOUNTS_READ,
-          permission: PERMISSIONS.ADMIN_READ + "|" + PERMISSIONS.ADMIN_WRITE,
         },
       ],
     },
     {
       roleId: 4,
       name: ROLES.ACCOUNTS_WRITER,
-      permission: PERMISSIONS.ADMIN_READ + "|" + PERMISSIONS.ADMIN_WRITE,
       permissions: [
         {
           permissionId: 3,
           name: PERMISSIONS.ACCOUNTS_READ,
-          permission: PERMISSIONS.ADMIN_READ + "|" + PERMISSIONS.ADMIN_WRITE,
         },
         {
           permissionId: 4,
           name: PERMISSIONS.ACCOUNTS_WRITE,
-          permission: PERMISSIONS.ADMIN_READ + "|" + PERMISSIONS.ADMIN_WRITE,
         },
       ],
     },
